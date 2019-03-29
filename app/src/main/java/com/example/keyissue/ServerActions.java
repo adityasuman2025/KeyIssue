a47b1bf30569cdf506cc4e80db44328e2d3ca200
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-03-30 00:39:56 +0530

    private keys done

--- app/src/main/java/com/example/keyissue/ServerActions.java
+++ app/src/main/java/com/example/keyissue/ServerActions.java
@@ -11,9 +11,9 @@ import java.net.URL;
 
 public class ServerActions  extends AsyncTask<String, Void, Bitmap>
 {
-    //String base_url = "http://172.16.26.43/key_issue_api/stud_img/";
+    String base_url = "http://172.16.26.43/key_issue_api/stud_img/";
 
-    String base_url = "http://mngo.in/key_issue_api/stud_img/";
+    //String base_url = "http://mngo.in/key_issue_api/stud_img/";
 
     @Override
     protected Bitmap doInBackground(String... params)
